a318b94ecf2321a268c00d78da5f730987844af3
filipmackiewicz <mackiewicz.filip@gmail.com>
2022-07-28 20:36:39 +0200

    added menu and routing

---
+++ src/modules/common/components/navbar/NavbarStyles.tsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+import Container from '@mui/material/Container';
+import { Link } from 'react-router-dom';
+
+export const CustomLink = styled(Link)`
+  color: #fff;
+  text-decoration: none;
+`;
